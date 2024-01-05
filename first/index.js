fbae6950dd462e6f16c44235c1d36f6e9d34dbf4
Haeni <haeng.gu723@gmail.com>
2024-01-05 13:48:09 +0900

    setting node.js

---
+++ first/index.js
@@ -0,0 +1,11 @@
+const express = require('express') //모듈 가져오기
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
