b98f37d92f56407a96239374a4d18fe84c9dd896
Haeni <haeng.gu723@gmail.com>
2024-01-07 20:41:58 +0900

    240107 node.js study

--- first/index.js
+++ first/index.js
@@ -2,6 +2,10 @@ const express = require('express') //모듈 가져오기
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://<password>@clustertest.u6po4pe.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
